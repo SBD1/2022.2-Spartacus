8021cbcaf4cac320a6f89cbd5ee4f343c96b38af
luclopesr <luclopesr@gmail.com>
2023-01-07 16:17:34 -0300

    Corrigindo arquivo ddl.sql
    Signed-off-by: luclopesr <luclopesr@gmail.com>

--- doc/Modulo_2/tabelas-DB.sql
+++ doc/Modulo_2/tabelas-DB.sql
@@ -1,88 +1,103 @@
 -- Scripts CREATE TABLE: TABELAS CRIADAS PARA O BANCO DE DADOS "Spartacus" ----
 -- AO TOTAL, SÃO 18 TABELAS --
 
-CREATE TABLE IF NOT EXISTS public."NPC"
+-- CREATE DATABASE Spartacus;
+
+CREATE TABLE IF NOT EXISTS NPC
 (
-    "idNPC" integer NOT NULL DEFAULT nextval('"NPC_idNPC_seq"'::regclass),
-    "NomeNPC" character varying(30) COLLATE pg_catalog."default" NOT NULL,
-    "Descricao" character varying(300) COLLATE pg_catalog."default",
-    "TipoNPC" character varying(10) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT "NPC_pkey" PRIMARY KEY ("idNPC")
+    idNPC serial NOT NULL,
+    NomeNPC varchar(30) NOT NULL,
+    Descricao varchar(300),
+    TipoNPC varchar(10) NOT NULL,
+    CONSTRAINT NPC_pkey PRIMARY KEY (idNPC)
 );
 
 --------------------------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS public."Item"
+CREATE TABLE IF NOT EXISTS Lugar
 (
-    "idItem" integer NOT NULL DEFAULT nextval('"Item_idItem_seq"'::regclass),
-    "Quantidade" integer NOT NULL,
-    "ValorItem" integer NOT NULL,
-    "TipoItem" character varying(15) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT "Item_pkey" PRIMARY KEY ("idItem")
+    idLugar serial NOT NULL,
+    NomeLugar varchar(30)  NOT NULL,
+    DescricaoLugar varchar(500)  NOT NULL,
+    CONSTRAINT Lugar_pkey PRIMARY KEY (idLugar)
 );
 
 --------------------------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS public."Amigável"
+CREATE TABLE IF NOT EXISTS Item
 (
-    "Fala" character varying(300) COLLATE pg_catalog."default" NOT NULL,
-    "idLoja" integer NOT NULL,
-    CONSTRAINT "Amigável_pkey" PRIMARY KEY ("idNPC"),
-    CONSTRAINT "idLoja_fk" FOREIGN KEY ("idLoja")
-        REFERENCES public."Loja" ("idLoja") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        DEFERRABLE INITIALLY DEFERRED
-        NOT VALID
-)
-    INHERITS (public."NPC");
-
---------------------------------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS public."Arma"
-(
-    "TipoArma" character varying(30) COLLATE pg_catalog."default" NOT NULL,
-    "Potencia" integer NOT NULL,
-    CONSTRAINT "Arma_pkey" PRIMARY KEY ("idItem")
-)
-    INHERITS (public."Item");
-
---------------------------------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS public."Armadura"
-(
-    "TipoArmadura" character varying(30) COLLATE pg_catalog."default" NOT NULL,
-    "Resistencia" integer NOT NULL,
-    CONSTRAINT "Armadura_pkey" PRIMARY KEY ("idItem")
-)
-    INHERITS (public."Item");
-
---------------------------------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS public."Batalha"
-(
-    "idBatalha" integer NOT NULL DEFAULT nextval('"Batalha_idBatalha_seq"'::regclass),
-    "QtdRespeitoBat" integer NOT NULL,
-    "QtdDinheiroBat" integer NOT NULL,
-    CONSTRAINT "Batalha_pkey" PRIMARY KEY ("idBatalha")
+    idItem serial NOT NULL,
+    Quantidade integer NOT NULL, 
+    ValorItem integer NOT NULL,
+    TipoItem varchar(15) NOT NULL,
+    CONSTRAINT Item_pkey PRIMARY KEY (idItem)
 );
 
 --------------------------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS public."Guerreiro"
+CREATE TABLE IF NOT EXISTS Amigável
 (
-    "idGuerreiro" integer NOT NULL DEFAULT nextval('"Guerreiro_idGuerreiro_seq"'::regclass),
-    "Nome" character varying(30) COLLATE pg_catalog."default" NOT NULL,
-    "Dinheiro" integer NOT NULL,
-    "Vida" integer NOT NULL,
-    "Forca" integer NOT NULL,
-    "Defesa" integer NOT NULL,
-    "Respeito" integer NOT NULL,
-    "Habilidade" integer NOT NULL,
-    "idLugar" integer NOT NULL,
-    CONSTRAINT "Guerreiro_pkey" PRIMARY KEY ("idGuerreiro"),
-    CONSTRAINT "idLugar_fk" FOREIGN KEY ("idLugar")
-        REFERENCES public."Lugar" ("idLugar") MATCH SIMPLE
+    Fala varchar(300)  NOT NULL,
+    CONSTRAINT Amigável_pkey PRIMARY KEY (idNPC)
+)
+    INHERITS (NPC);
+
+--------------------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS Arma
+(
+    TipoArma varchar(30)  NOT NULL,
+    Potencia integer NOT NULL,
+    CONSTRAINT Arma_pkey PRIMARY KEY (idItem)
+)
+    INHERITS (Item);
+
+--------------------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS Armadura
+(
+    TipoArmadura varchar(30) NOT NULL,
+    Resistencia integer NOT NULL,
+    CONSTRAINT Armadura_pkey PRIMARY KEY (idItem)
+)
+    INHERITS (Item);
+    
+--------------------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS Poção
+(
+    TipoPoc varchar  NOT NULL,
+    QtdPoc integer NOT NULL,
+    CONSTRAINT Poção_pkey PRIMARY KEY (idItem)
+)
+    INHERITS (Item);
+
+--------------------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS Batalha
+(
+    idBatalha serial NOT NULL,
+    QtdRespeitoBat integer NOT NULL,
+    QtdDinheiroBat integer NOT NULL,
+    CONSTRAINT Batalha_pkey PRIMARY KEY (idBatalha)
+);
+
+--------------------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS Guerreiro
+(
+    idGuerreiro serial NOT NULL,
+    Nome varchar(30)  NOT NULL,
+    Dinheiro integer NOT NULL,
+    Vida integer NOT NULL,
+    Forca integer NOT NULL,
+    Defesa integer NOT NULL,
+    Respeito integer NOT NULL,
+    Habilidade integer NOT NULL,
+    idLugar integer NOT NULL,
+    CONSTRAINT Guerreiro_pkey PRIMARY KEY (idGuerreiro),
+    CONSTRAINT idLugar_fk FOREIGN KEY (idLugar)
+        REFERENCES Lugar (idLugar) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE NO ACTION
         DEFERRABLE INITIALLY DEFERRED
@@ -91,63 +106,60 @@ CREATE TABLE IF NOT EXISTS public."Guerreiro"
 
 --------------------------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS public."Inimigo"
+CREATE TABLE IF NOT EXISTS Loja
 (
-    "Dano" integer NOT NULL,
-    "Vida" integer NOT NULL,
-    "idLugar" integer NOT NULL,
-    CONSTRAINT "Inimigo_pkey" PRIMARY KEY ("idNPC"),
-    CONSTRAINT "idLugar_fk" FOREIGN KEY ("idLugar")
-        REFERENCES public."Lugar" ("idLugar") MATCH SIMPLE
+    idLoja serial NOT NULL,
+    Nome varchar(15)  NOT NULL,
+    Descricao varchar(300)  NOT NULL,
+    idAmigavel integer NOT NULL,
+    idLugar integer NOT NULL,
+    CONSTRAINT Loja_pkey PRIMARY KEY (idLoja),
+    CONSTRAINT idAmigavel_fk FOREIGN KEY (idAmigavel)
+        REFERENCES Amigável (idNPC) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        DEFERRABLE INITIALLY DEFERRED
+        NOT VALID,
+    CONSTRAINT idLugar_fk FOREIGN KEY (idLugar)
+        REFERENCES Lugar (idLugar) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        DEFERRABLE INITIALLY DEFERRED
+);
+
+--------------------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS Inimigo
+(
+    Dano integer NOT NULL,
+    Vida integer NOT NULL,
+    idLugar integer NOT NULL,
+    CONSTRAINT Inimigo_pkey PRIMARY KEY (idNPC),
+    CONSTRAINT idLugar_fk FOREIGN KEY (idLugar)
+        REFERENCES Lugar (idLugar) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         DEFERRABLE INITIALLY DEFERRED
         NOT VALID
 )
-    INHERITS (public."NPC");
+    INHERITS (NPC);
 
 --------------------------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS public."Instância de Batalha"
+CREATE TABLE IF NOT EXISTS Instancia_de_Inimigo
 (
-    "idInstancia" integer NOT NULL DEFAULT nextval('"Instância de Batalha_idInstancia_seq"'::regclass),
-    "idBatalha" integer NOT NULL,
-    "idInstInimigo" integer NOT NULL,
-    "idGuerreiro" integer NOT NULL,
-    CONSTRAINT "Instância de Batalha_pkey" PRIMARY KEY ("idInstancia"),
-    CONSTRAINT "idBatalha_fk" FOREIGN KEY ("idBatalha")
-        REFERENCES public."Batalha" ("idBatalha") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        DEFERRABLE INITIALLY DEFERRED,
-    CONSTRAINT "idGuerreiro_fk" FOREIGN KEY ("idGuerreiro")
-        REFERENCES public."Guerreiro" ("idGuerreiro") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        DEFERRABLE INITIALLY DEFERRED,
-    CONSTRAINT "idInstInimigo_fk" FOREIGN KEY ("idInstInimigo")
-        REFERENCES public."Instância de Inimigo" ("idInstancia") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        DEFERRABLE INITIALLY DEFERRED
-);
-
---------------------------------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS public."Instância de Inimigo"
-(
-    "idInstancia" integer NOT NULL DEFAULT nextval('"Instância de Inimigo_idInstancia_seq"'::regclass),
-    "idInimigo" integer NOT NULL,
-    "idGuerreiro" integer NOT NULL,
-    CONSTRAINT "Instância de Inimigo_pkey" PRIMARY KEY ("idInstancia"),
-    CONSTRAINT "idGuerreiro_fk" FOREIGN KEY ("idGuerreiro")
-        REFERENCES public."Guerreiro" ("idGuerreiro") MATCH SIMPLE
+    idInstancia serial NOT NULL,
+    idInimigo integer NOT NULL,
+    idGuerreiro integer NOT NULL,
+    CONSTRAINT Instancia_de_Inimigo_pkey PRIMARY KEY (idInstancia),
+    CONSTRAINT idGuerreiro_fk FOREIGN KEY (idGuerreiro)
+        REFERENCES Guerreiro (idGuerreiro) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         DEFERRABLE INITIALLY DEFERRED
         NOT VALID,
-    CONSTRAINT "idInimigo_fk" FOREIGN KEY ("idInimigo")
-        REFERENCES public."Inimigo" ("idNPC") MATCH SIMPLE
+    CONSTRAINT idInimigo_fk FOREIGN KEY (idInimigo)
+        REFERENCES Inimigo (idNPC) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         DEFERRABLE INITIALLY DEFERRED
@@ -155,31 +167,25 @@ CREATE TABLE IF NOT EXISTS public."Instância de Inimigo"
 
 --------------------------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS public."Instância de Item"
+CREATE TABLE IF NOT EXISTS Instancia_de_Batalha
 (
-    "idInstancia" integer NOT NULL DEFAULT nextval('"Instância de Item_idInstancia_seq"'::regclass),
-    "idItem" integer NOT NULL,
-    "idGuerreiro" integer NOT NULL,
-    "idLugar" integer,
-    "idLoja" integer,
-    CONSTRAINT "Instância de Item_pkey" PRIMARY KEY ("idInstancia"),
-    CONSTRAINT "idGuerreiro_fk" FOREIGN KEY ("idGuerreiro")
-        REFERENCES public."Guerreiro" ("idGuerreiro") MATCH SIMPLE
+    idInstancia serial NOT NULL,
+    idBatalha integer NOT NULL,
+    idInstInimigo integer NOT NULL,
+    idGuerreiro integer NOT NULL,
+    CONSTRAINT Instancia_de_Batalha_pkey PRIMARY KEY (idInstancia),
+    CONSTRAINT idBatalha_fk FOREIGN KEY (idBatalha)
+        REFERENCES Batalha (idBatalha) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         DEFERRABLE INITIALLY DEFERRED,
-    CONSTRAINT "idItem_fk" FOREIGN KEY ("idItem")
-        REFERENCES public."Item" ("idItem") MATCH SIMPLE
+    CONSTRAINT idGuerreiro_fk FOREIGN KEY (idGuerreiro)
+        REFERENCES Guerreiro (idGuerreiro) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         DEFERRABLE INITIALLY DEFERRED,
-    CONSTRAINT "idLoja_fk" FOREIGN KEY ("idLoja")
-        REFERENCES public."Loja" ("idLoja") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        DEFERRABLE INITIALLY DEFERRED,
-    CONSTRAINT "idLugar_fk" FOREIGN KEY ("idLugar")
-        REFERENCES public."Lugar" ("idLugar") MATCH SIMPLE
+    CONSTRAINT idInstInimigo_fk FOREIGN KEY (idInstInimigo)
+        REFERENCES Instancia_de_Inimigo (idInstancia) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         DEFERRABLE INITIALLY DEFERRED
@@ -187,31 +193,31 @@ CREATE TABLE IF NOT EXISTS public."Instância de Item"
 
 --------------------------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS public."Instância de Missão"
+CREATE TABLE IF NOT EXISTS Instancia_de_Item
 (
-    "idInstancia" integer NOT NULL DEFAULT nextval('"Instância de Missão_idInstancia_seq"'::regclass),
-    "idMissao" integer NOT NULL,
-    "idAmigavel" integer NOT NULL,
-    "idInimigo" integer NOT NULL,
-    "idGuerreiro" integer NOT NULL,
-    CONSTRAINT "Instância de Missão_pkey" PRIMARY KEY ("idInstancia"),
-    CONSTRAINT "idAmigavel_fk" FOREIGN KEY ("idAmigavel")
-        REFERENCES public."Amigável" ("idNPC") MATCH SIMPLE
+    idInstancia serial NOT NULL,
+    idItem integer NOT NULL,
+    idGuerreiro integer NOT NULL,
+    idLugar integer,
+    idLoja integer,
+    CONSTRAINT Instancia_de_Item_pkey PRIMARY KEY (idInstancia),
+    CONSTRAINT idGuerreiro_fk FOREIGN KEY (idGuerreiro)
+        REFERENCES Guerreiro (idGuerreiro) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         DEFERRABLE INITIALLY DEFERRED,
-    CONSTRAINT "idGuerreiro_fk" FOREIGN KEY ("idGuerreiro")
-        REFERENCES public."Guerreiro" ("idGuerreiro") MATCH SIMPLE
+    CONSTRAINT idItem_fk FOREIGN KEY (idItem)
+        REFERENCES Item (idItem) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         DEFERRABLE INITIALLY DEFERRED,
-    CONSTRAINT "idInimigo_fk" FOREIGN KEY ("idInimigo")
-        REFERENCES public."Inimigo" ("idNPC") MATCH SIMPLE
+    CONSTRAINT idLoja_fk FOREIGN KEY (idLoja)
+        REFERENCES Loja (idLoja) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         DEFERRABLE INITIALLY DEFERRED,
-    CONSTRAINT "idMissao_fk" FOREIGN KEY ("idMissao")
-        REFERENCES public."Missao" ("idMissao") MATCH SIMPLE
+    CONSTRAINT idLugar_fk FOREIGN KEY (idLugar)
+        REFERENCES Lugar (idLugar) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         DEFERRABLE INITIALLY DEFERRED
@@ -219,19 +225,43 @@ CREATE TABLE IF NOT EXISTS public."Instância de Missão"
 
 --------------------------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS public."Instância de Treino"
+CREATE TABLE IF NOT EXISTS Missao
 (
-    "idTreino" integer NOT NULL,
-    "idGuerreiro" integer NOT NULL,
-    "idInstancia" integer NOT NULL DEFAULT nextval('"Instância de Treino_idInstancia_seq"'::regclass),
-    CONSTRAINT "Instância de Treino_pkey" PRIMARY KEY ("idInstancia"),
-    CONSTRAINT "idGuerreiro_fk" FOREIGN KEY ("idGuerreiro")
-        REFERENCES public."Guerreiro" ("idGuerreiro") MATCH SIMPLE
+    idMissao serial NOT NULL,
+    NomeMissao varchar(50)  NOT NULL,
+    DescricaoMissao varchar(300)  NOT NULL,
+    Objetivo varchar(150)  NOT NULL,
+    Recompensa integer NOT NULL,
+    CONSTRAINT Missao_pkey PRIMARY KEY (idMissao)
+);
+
+--------------------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS Instancia_de_Missao
+(
+    idInstancia serial NOT NULL,
+    idMissao integer NOT NULL,
+    idAmigavel integer NOT NULL,
+    idInimigo integer NOT NULL,
+    idGuerreiro integer NOT NULL,
+    CONSTRAINT Instancia_de_Missao_pkey PRIMARY KEY (idInstancia),
+    CONSTRAINT idAmigavel_fk FOREIGN KEY (idAmigavel)
+        REFERENCES Amigável (idNPC) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         DEFERRABLE INITIALLY DEFERRED,
-    CONSTRAINT "idTreino_fk" FOREIGN KEY ("idTreino")
-        REFERENCES public."Treino" ("idTreino") MATCH SIMPLE
+    CONSTRAINT idGuerreiro_fk FOREIGN KEY (idGuerreiro)
+        REFERENCES Guerreiro (idGuerreiro) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        DEFERRABLE INITIALLY DEFERRED,
+    CONSTRAINT idInimigo_fk FOREIGN KEY (idInimigo)
+        REFERENCES Inimigo (idNPC) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        DEFERRABLE INITIALLY DEFERRED,
+    CONSTRAINT idMissao_fk FOREIGN KEY (idMissao)
+        REFERENCES Missao (idMissao) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         DEFERRABLE INITIALLY DEFERRED
@@ -239,72 +269,33 @@ CREATE TABLE IF NOT EXISTS public."Instância de Treino"
 
 --------------------------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS public."Loja"
+CREATE TABLE IF NOT EXISTS Treino
 (
-    "idLoja" integer NOT NULL DEFAULT nextval('"Loja_idLoja_seq"'::regclass),
-    "Nome" character varying(15) COLLATE pg_catalog."default" NOT NULL,
-    "Descricao" character varying(300) COLLATE pg_catalog."default" NOT NULL,
-    "idAmigavel" integer NOT NULL,
-    "idLugar" integer NOT NULL,
-    CONSTRAINT "Loja_pkey" PRIMARY KEY ("idLoja"),
-    CONSTRAINT "idAmigavel_fk" FOREIGN KEY ("idAmigavel")
-        REFERENCES public."Amigável" ("idNPC") MATCH SIMPLE
+    idTreino serial NOT NULL,
+    TipoTreino varchar(15)  NOT NULL,
+    ValorTreino integer NOT NULL,
+    MelhoriaTreino integer NOT NULL,
+    CONSTRAINT Treino_pkey PRIMARY KEY (idTreino)
+);
+
+--------------------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS Instancia_de_Treino
+(
+    idInstancia serial NOT NULL,
+    idTreino integer NOT NULL,
+    idGuerreiro integer NOT NULL,
+    CONSTRAINT Instancia_de_Treino_pkey PRIMARY KEY (idInstancia),
+    CONSTRAINT idGuerreiro_fk FOREIGN KEY (idGuerreiro)
+        REFERENCES Guerreiro (idGuerreiro) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        DEFERRABLE INITIALLY DEFERRED,
+    CONSTRAINT idTreino_fk FOREIGN KEY (idTreino)
+        REFERENCES Treino (idTreino) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         DEFERRABLE INITIALLY DEFERRED
-        NOT VALID,
-    CONSTRAINT "idLugar_fk" FOREIGN KEY ("idLugar")
-        REFERENCES public."Lugar" ("idLugar") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        DEFERRABLE INITIALLY DEFERRED
-);
-
---------------------------------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS public."Lugar"
-(
-    "idLugar" integer NOT NULL DEFAULT nextval('"Lugar_idLugar_seq"'::regclass),
-    "NomeLugar" character varying(30) COLLATE pg_catalog."default" NOT NULL,
-    "DescricaoLugar" character varying(500) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT "Lugar_pkey" PRIMARY KEY ("idLugar")
-);
-
---------------------------------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS public."Missao"
-(
-    "idMissao" integer NOT NULL DEFAULT nextval('"Missao_idMissao_seq"'::regclass),
-    "NomeMissao" character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    "DescricaoMissao" character varying(300) COLLATE pg_catalog."default" NOT NULL,
-    "Objetivo" character varying(150) COLLATE pg_catalog."default" NOT NULL,
-    "Recompensa" integer NOT NULL,
-    CONSTRAINT "Missao_pkey" PRIMARY KEY ("idMissao")
-);
-
---------------------------------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS public."Poção"
-(
-    -- Inherited from table public."Item": "idItem" integer NOT NULL DEFAULT nextval('"Item_idItem_seq"'::regclass),
-    -- Inherited from table public."Item": "Quantidade" integer NOT NULL,
-    -- Inherited from table public."Item": "ValorItem" integer NOT NULL,
-    -- Inherited from table public."Item": "TipoItem" character varying(15) COLLATE pg_catalog."default" NOT NULL,
-    "TipoPoc" character varying COLLATE pg_catalog."default" NOT NULL,
-    "QtdPoc" integer NOT NULL,
-    CONSTRAINT "Poção_pkey" PRIMARY KEY ("idItem")
-)
-    INHERITS (public."Item");
-
---------------------------------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS public."Treino"
-(
-    "idTreino" integer NOT NULL DEFAULT nextval('"Treino_idTreino_seq"'::regclass),
-    "TipoTreino" character varying(15) COLLATE pg_catalog."default" NOT NULL,
-    "ValorTreino" integer NOT NULL,
-    "MelhoriaTreino" integer NOT NULL,
-    CONSTRAINT "Treino_pkey" PRIMARY KEY ("idTreino")
 );
 
 --------------------------------------------------------------------------
